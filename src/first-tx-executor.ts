cecfd10ac5f8aef4005ded3e68cd418b25f348b7
vlad1chek <vladbarcelonafc@gmail.com>
2022-10-14 13:29:28 +0300

    feat: add new events handling + gas

--- src/first-tx-executor.ts
+++ src/first-tx-executor.ts
@@ -2,35 +2,67 @@ import {
   AdminUpdated as AdminUpdatedEvent,
   CCSmartWalletAddressUpdated as CCSmartWalletAddressUpdatedEvent,
   FirstPartOfSwapExecuted as FirstPartOfSwapExecutedEvent,
-} from "../generated/FirstTxExecutor/FirstTxExecutor"
-import { AdminUpdated, CCSmartWalletAddressUpdated, FirstPartOfSwapExecuted } from "../generated/schema"
-
+  DirectUSDCTransfer as DirectUSDCTransferEvent,
+} from "../generated/FirstTxExecutor/FirstTxExecutor";
+import {
+  AdminUpdated,
+  CCSmartWalletAddressUpdated,
+  FirstPartOfSwapExecuted,
+  DirectUSDCTransfer,
+} from "../generated/schema";
 
 export function handleAdminUpdated(event: AdminUpdatedEvent): void {
-  let entity = new AdminUpdated(event.transaction.hash.toHex() + "-" + event.logIndex.toString())
-  entity.oldAdmin = event.params.oldAdmin
-  entity.newAdmin = event.params.newAdmin
-  entity.save()
+  let entity = new AdminUpdated(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  );
+  entity.oldAdmin = event.params.oldAdmin;
+  entity.newAdmin = event.params.newAdmin;
+  entity.save();
 }
 
-export function handleCCSmartWalletAddressUpdated(event: CCSmartWalletAddressUpdatedEvent): void {
-  let entity = new CCSmartWalletAddressUpdated(event.transaction.hash.toHex() + "-" + event.logIndex.toString())
-  entity.oldAddress = event.params.oldAddress
-  entity.newAddress = event.params.newAddress
-  entity.save()
+export function handleCCSmartWalletAddressUpdated(
+  event: CCSmartWalletAddressUpdatedEvent
+): void {
+  let entity = new CCSmartWalletAddressUpdated(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  );
+  entity.oldAddress = event.params.oldAddress;
+  entity.newAddress = event.params.newAddress;
+  entity.save();
 }
 
-export function handleFirstPartOfSwapExecuted(event: FirstPartOfSwapExecutedEvent): void {
-  let entity = new FirstPartOfSwapExecuted(event.transaction.hash.toHex() + "-" + event.logIndex.toString())
-  entity.destAmount = event.params.destAmount
-  entity.destNetworkId = event.params.destNetworkId
-  entity.destTokenAddress = event.params.destTokenAddress
-  entity.destUserAddress = event.params.destUserAddress
-  entity.slippage = event.params.slippage
-  entity.initiator = event.transaction.from;
-  entity.srcTransactionHash = event.transaction.hash
+export function handleDirectUSDCTransfer(event: DirectUSDCTransferEvent): void {
+  let entity = new DirectUSDCTransfer(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  );
+  entity.amount = event.params.amount;
+  entity.userAddress = event.params.userAddress;
+  entity.smartWallet = event.params.smartWallet;
   entity.blockHash = event.block.hash;
   entity.blockNumber = event.block.number;
   entity.timestamp = event.block.timestamp;
-  entity.save()
+  entity.txGasPrice = event.transaction.gasPrice;
+  entity.txGasUsed = event.transaction.gasLimit;
+  entity.save();
+}
+
+export function handleFirstPartOfSwapExecuted(
+  event: FirstPartOfSwapExecutedEvent
+): void {
+  let entity = new FirstPartOfSwapExecuted(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  );
+  entity.destAmount = event.params.destAmount;
+  entity.destNetworkId = event.params.destNetworkId;
+  entity.destTokenAddress = event.params.destTokenAddress;
+  entity.destUserAddress = event.params.destUserAddress;
+  entity.slippage = event.params.slippage;
+  entity.initiator = event.transaction.from;
+  entity.srcTransactionHash = event.transaction.hash;
+  entity.txGasPrice = event.transaction.gasPrice;
+  entity.txGasUsed = event.transaction.gasLimit;
+  entity.blockHash = event.block.hash;
+  entity.blockNumber = event.block.number;
+  entity.timestamp = event.block.timestamp;
+  entity.save();
 }
