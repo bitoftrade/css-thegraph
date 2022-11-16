27b7d89ecbf39ff310fc08369dec3cfe00ec34a0
Danylo <danylo@code-care.pro>
2022-11-16 17:58:52 +0200

    Add TxHash & fix bug with array of addresses

--- src/validator-owners-registry.ts
+++ src/validator-owners-registry.ts
@@ -10,11 +10,13 @@ import {
   ValidatorAddressAdded,
   ValidatorsOwnersAdded,
 } from "../generated/schema";
+import { Bytes } from '@graphprotocol/graph-ts'
 
 export function handleOwnershipTransferred(event: OwnershipTransferredEvent): void {
   let entity = new OwnershipTransferred(event.transaction.hash.toHex() + "-" + event.logIndex.toString());
   entity.previousOwner = event.params.previousOwner;
   entity.newOwner = event.params.newOwner;
+  entity.txHash = event.transaction.hash;
   entity.blockHash = event.block.hash;
   entity.blockNumber = event.block.number;
   entity.timestamp = event.block.timestamp;
@@ -24,6 +26,7 @@ export function handleOwnershipTransferred(event: OwnershipTransferredEvent): vo
 export function handleOracleUpdated(event: OracleUpdatedEvent): void {
   let entity = new OracleUpdated(event.transaction.hash.toHex() + "-" + event.logIndex.toString());
   entity.oracle = event.params.oracle;
+  entity.txHash = event.transaction.hash;
   entity.blockHash = event.block.hash;
   entity.blockNumber = event.block.number;
   entity.timestamp = event.block.timestamp;
@@ -33,6 +36,7 @@ export function handleOracleUpdated(event: OracleUpdatedEvent): void {
 export function handleValidatorAddressAdded(event: ValidatorAddressAddedEvent): void {
   let entity = new ValidatorAddressAdded(event.transaction.hash.toHex() + "-" + event.logIndex.toString());
   entity.validatorAddress = event.params.validatorAddress;
+  entity.txHash = event.transaction.hash;
   entity.blockHash = event.block.hash;
   entity.blockNumber = event.block.number;
   entity.timestamp = event.block.timestamp;
@@ -41,9 +45,14 @@ export function handleValidatorAddressAdded(event: ValidatorAddressAddedEvent): 
 
 export function handleValidatorsOwnersAdded(event: ValidatorsOwnersAddedEvent): void {
   let entity = new ValidatorsOwnersAdded(event.transaction.hash.toHex() + "-" + event.logIndex.toString());
+
+  let validatorsOwners = new Array<Bytes>(); 
   for (let i = 0; i < event.params.validatorsOwners.length; i++) {
-      entity.validatorsOwners.push(event.params.validatorsOwners[i])
+      validatorsOwners.push(event.params.validatorsOwners[i]);
   }
+  entity.validatorsOwners = validatorsOwners; 
+
+  entity.txHash = event.transaction.hash;
   entity.blockHash = event.block.hash;
   entity.blockNumber = event.block.number;
   entity.timestamp = event.block.timestamp;
