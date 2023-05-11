6f75a58ce72e5dbeceda674521b7719a722f4650
Danylo <danylo@code-care.pro>
2023-05-11 18:13:54 +0300

    feat: add filter for multichain transfer by wallet addresses

--- src/multichain-router.ts
+++ src/multichain-router.ts
@@ -1,22 +1,33 @@
 import { MultichainTransfer } from '../generated/schema';
 import { LogAnySwapIn as LogAnySwapInEvent } from "../generated/MultichainRouter/MultichainRouter";
 
+// filter events by wallet address
+const wallets = ['0x6d569eA24622540a63D589C10d5Ba899f8731409'].map(
+  function toLowerCase(a: string, index: i32, array: string[]): string {
+    return a.toLowerCase()
+  })
+
 export function handleMultichainTransfer(event: LogAnySwapInEvent): void {
-  let entity = new MultichainTransfer(event.transaction.hash.toHex() + "-" + event.logIndex.toString());
+  const receiver = event.params.to.toHexString().toLowerCase()
 
-  entity.srcTransactionHash = event.params.txhash;
-  entity.anyToken = event.params.token;
-  entity.to = event.params.to;
-  entity.amount = event.params.amount;
-  entity.fromChainId = event.params.fromChainID;
-  entity.toChainId = event.params.toChainID;
+  if (wallets.includes(receiver)) {
 
-  entity.txHash = event.transaction.hash;
-  entity.txGasPrice = event.transaction.gasPrice;
-  entity.txGasUsed = event.transaction.gasLimit;
-  entity.blockHash = event.block.hash;
-  entity.blockNumber = event.block.number;
-  entity.timestamp = event.block.timestamp;
+    let entity = new MultichainTransfer(event.transaction.hash.toHex() + "-" + event.logIndex.toString());
 
-  entity.save();
+    entity.srcTransactionHash = event.params.txhash;
+    entity.anyToken = event.params.token;
+    entity.to = event.params.to;
+    entity.amount = event.params.amount;
+    entity.fromChainId = event.params.fromChainID;
+    entity.toChainId = event.params.toChainID;
+
+    entity.txHash = event.transaction.hash;
+    entity.txGasPrice = event.transaction.gasPrice;
+    entity.txGasUsed = event.transaction.gasLimit;
+    entity.blockHash = event.block.hash;
+    entity.blockNumber = event.block.number;
+    entity.timestamp = event.block.timestamp;
+
+    entity.save();
+  }
 }
