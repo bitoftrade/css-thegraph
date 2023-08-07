722e180138c61d3dc22db1157d0a339335b5991e
Danylo <danylo@code-care.pro>
2023-08-07 11:21:12 +0300

    feat: add StargateTransferReceived event listener

--- src/cross-chain-router.ts
+++ src/cross-chain-router.ts
@@ -1,8 +1,12 @@
-import { SrcCrossSwap as SrcCrossSwapEvent, SrcCrossSwapV2 as SrcCrossSwapV2Event } from "../generated/CrossChainRouter/CrossChainRouter";
-import { SrcCrossSwap } from "../generated/schema";
+import { BigInt } from '@graphprotocol/graph-ts';
+import {
+  SrcCrossSwap as SrcCrossSwapEvent,
+  SrcCrossSwapV2 as SrcCrossSwapV2Event,
+} from '../generated/CrossChainRouter/CrossChainRouter';
+import { SrcCrossSwap } from '../generated/schema';
 
 export function handleSrcCrossSwap(event: SrcCrossSwapEvent): void {
-  let entity = new SrcCrossSwap(event.transaction.hash.toHex() + "-" + event.logIndex.toString());
+  let entity = new SrcCrossSwap(event.transaction.hash.toHex() + '-' + event.logIndex.toString());
 
   entity.srcToken = event.params.srcToken;
   entity.srcAmount = event.params.srcAmount;
@@ -14,9 +18,13 @@ export function handleSrcCrossSwap(event: SrcCrossSwapEvent): void {
   entity.connectorTokenIncome = event.params.connectorTokenIncome;
   entity.refundAddress = event.params.refundAddress;
   entity.liquidityProvider = event.params.liquidityProvider;
+
+  // default value, not used in this version
+  entity.nonce = new BigInt(0);
+  // legacy version, initiator is event.transaction.from
   entity.initiator = event.transaction.from;
 
-  entity.version = "1.1";
+  entity.version = '1.0';
 
   entity.txHash = event.transaction.hash;
   entity.txGasPrice = event.transaction.gasPrice;
@@ -28,7 +36,7 @@ export function handleSrcCrossSwap(event: SrcCrossSwapEvent): void {
 }
 
 export function handleSrcCrossSwapV2(event: SrcCrossSwapV2Event): void {
-  let entity = new SrcCrossSwap(event.transaction.hash.toHex() + "-" + event.logIndex.toString());
+  let entity = new SrcCrossSwap(event.transaction.hash.toHex() + '-' + event.logIndex.toString());
 
   entity.srcToken = event.params.srcToken;
   entity.srcAmount = event.params.srcAmount;
@@ -40,10 +48,12 @@ export function handleSrcCrossSwapV2(event: SrcCrossSwapV2Event): void {
   entity.connectorTokenIncome = event.params.connectorTokenIncome;
   entity.refundAddress = event.params.refundAddress;
   entity.liquidityProvider = event.params.liquidityProvider;
-  // here initiator is not event.transaction.from, but event.params.initiator 
+
+  entity.nonce = event.params.nonce;
+  // here initiator is not event.transaction.from, but event.params.initiator
   entity.initiator = event.params.initiator;
 
-  entity.version = "1.2";
+  entity.version = '2.0';
 
   entity.txHash = event.transaction.hash;
   entity.txGasPrice = event.transaction.gasPrice;
@@ -53,4 +63,3 @@ export function handleSrcCrossSwapV2(event: SrcCrossSwapV2Event): void {
   entity.timestamp = event.block.timestamp;
   entity.save();
 }
-
--- src/cross-chain-wallet.ts
+++ src/cross-chain-wallet.ts
@@ -1,9 +1,13 @@
+import {
+  DestCrossSwap as DestCrossSwapEvent,
+  DestCrossSwapV2 as DestCrossSwapV2Event,
+  StargateTransferReceived as StargateTransferReceivedEvent,
+} from '../generated/CrossChainWallet/CrossChainWallet';
+import { DestCrossSwap, StargateTransferReceived } from '../generated/schema';
 
-import { DestCrossSwap as DestCrossSwapEvent } from "../generated/CrossChainWallet/CrossChainWallet";
-import { DestCrossSwap } from "../generated/schema";
 
 export function handleDestCrossSwap(event: DestCrossSwapEvent): void {
-  let entity = new DestCrossSwap(event.transaction.hash.toHex() + "-" + event.logIndex.toString());
+  let entity = new DestCrossSwap(event.transaction.hash.toHex() + '-' + event.logIndex.toString());
 
   entity.srcChainId = event.params.srcChainId;
   entity.srcTransactionHash = event.params.srcTransactionHash;
@@ -14,9 +18,11 @@ export function handleDestCrossSwap(event: DestCrossSwapEvent): void {
   entity.protocolFee = event.params.protocolFee;
   entity.gasCompensation = event.params.gasCompensation;
   entity.liquidityProvider = event.params.liquidityProvider;
+
+  // legacy version, initiator is event.transaction.from
   entity.initiator = event.transaction.from;
 
-  entity.version = "1.1";
+  entity.version = '2.0';
 
   entity.txHash = event.transaction.hash;
   entity.txGasPrice = event.transaction.gasPrice;
@@ -24,5 +30,51 @@ export function handleDestCrossSwap(event: DestCrossSwapEvent): void {
   entity.blockHash = event.block.hash;
   entity.blockNumber = event.block.number;
   entity.timestamp = event.block.timestamp;
+  entity.save();
+}
+
+export function handleDestCrossSwapV2(event: DestCrossSwapV2Event): void {
+  let entity = new DestCrossSwap(event.transaction.hash.toHex() + '-' + event.logIndex.toString());
+
+  entity.srcChainId = event.params.srcChainId;
+  entity.srcTransactionHash = event.params.srcTransactionHash;
+  entity.destToken = event.params.destToken;
+  entity.destAmount = event.params.destAmount;
+  entity.destUser = event.params.destUser;
+  entity.connectorToken = event.params.connectorToken;
+  entity.protocolFee = event.params.protocolFee;
+  entity.gasCompensation = event.params.gasCompensation;
+  entity.liquidityProvider = event.params.liquidityProvider;
+
+  // here initiator is not event.transaction.from, but event.params.initiator
+  entity.initiator = event.params.initiator;
+
+  entity.version = '2.1';
+
+  entity.txHash = event.transaction.hash;
+  entity.txGasPrice = event.transaction.gasPrice;
+  entity.txGasUsed = event.transaction.gasLimit;
+  entity.blockHash = event.block.hash;
+  entity.blockNumber = event.block.number;
+  entity.timestamp = event.block.timestamp;
+  entity.save();
+}
+
+export function handleStargateTransferReceived(event: StargateTransferReceivedEvent): void {
+  let entity = new StargateTransferReceived(event.transaction.hash.toHex() + '-' + event.logIndex.toString());
+
+  entity.sgChainId = event.params.sgChainId;
+  entity.token = event.params.token;
+  entity.amount = event.params.amount;
+  entity.nonce = event.params.nonce;
+  entity.sgNonce = event.params.sgNonce;
+
+  entity.txHash = event.transaction.hash;
+  entity.txGasUsed = event.transaction.gasLimit;
+  entity.txGasPrice = event.transaction.gasPrice;
+  entity.blockHash = event.block.hash;
+  entity.blockNumber = event.block.number;
+  entity.timestamp = event.block.timestamp;
+
   entity.save();
 }
