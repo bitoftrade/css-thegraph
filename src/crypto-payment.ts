fb62a1b410fd484871ef809a00b284fd4e94cab9
Nazarii Kyselevych <nazarkysel@gmail.com>
2023-09-21 11:48:13 +0300

    feat: backward compatibility

--- src/crypto-payment.ts
+++ src/crypto-payment.ts
@@ -1,7 +1,37 @@
-import { PaymentV2 as PaymentEvent } from "../generated/PaymentGateway/PaymentGateway";
+import {
+  Payment as PaymentEvent,
+  PaymentV2 as PaymentV2Event,
+} from "../generated/PaymentGateway/PaymentGateway";
 import { Payment } from "../generated/schema";
+import { BigInt } from "@graphprotocol/graph-ts";
 
 export function handlePayment(event: PaymentEvent): void {
+  let entity = new Payment(event.transaction.hash.toHex() + "-" + event.logIndex.toString());
+
+  // event
+  entity.orderId = event.params.orderId;
+  entity.payInToken = event.params.payInToken;
+  entity.payOutToken = event.params.payOutToken;
+  entity.payInAmount = event.params.payInAmount;
+  entity.payOutAmount = event.params.payOutAmount;
+  entity.protocolFeeAmount = new BigInt(0);
+  entity.merchant = event.params.merchant;
+
+  // metadata
+  entity.initiator = event.transaction.from;
+  entity.txGasUsed = event.transaction.gasLimit;
+  entity.txGasPrice = event.transaction.gasPrice;
+  entity.blockHash = event.block.hash;
+  entity.blockNumber = event.block.number;
+  entity.timestamp = event.block.timestamp;
+  entity.txHash = event.transaction.hash;
+
+  entity.version = "1.0";
+
+  entity.save();
+}
+
+export function handlePaymentV2(event: PaymentV2Event): void {
   let entity = new Payment(event.transaction.hash.toHex() + "-" + event.logIndex.toString());
 
   // event
@@ -21,6 +51,8 @@ export function handlePayment(event: PaymentEvent): void {
   entity.blockNumber = event.block.number;
   entity.timestamp = event.block.timestamp;
   entity.txHash = event.transaction.hash;
+
+  entity.version = "2.0";
 
   entity.save();
 }
